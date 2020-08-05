0ee6e5e4a9ad2c5eab78bb33204a827d0a1119e2
Shannon Trainor <sltrainor@outlook.com>
2020-08-05 16:22:07 -0400

    basic layout

---
+++ src/components/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+
+function Search() {
+    return (
+        <div>
+            <form>
+                <input placeholder='Search' />
+            </form>
+        </div>
+    )
+}
+
+export default Search;
