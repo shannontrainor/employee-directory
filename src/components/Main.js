fe12fe61ba567576a68ab707908b1540870b4902
Shannon Trainor <sltrainor@outlook.com>
2020-08-05 16:21:48 -0400

    basic layout with tabledata

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TableData from  './Table/table-data';
+
+
+
+function Main() {
+    return (
+        <>
+            <TableData/>
+        </>
+    )
+}
+
+export default Main;
