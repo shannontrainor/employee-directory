75e90e9bb887732da07a64b646c8ffb287bb58f4
Shannon Trainor <sltrainor@outlook.com>
2020-08-05 16:21:14 -0400

    added header, main, wrapper

--- src/App.js
+++ src/App.js
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Main from './components/Main';
+import Wrapper from './components/Wrapper';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Header/>
+      <Main/>
+    </Wrapper>
+  
   );
 }
 
 export default App;
+
+//app
+  //wrapper
+    //header - header of the page
+    //main 
+      //nav
+        //searchbox
+      //table folder
