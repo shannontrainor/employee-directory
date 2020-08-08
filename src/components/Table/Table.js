fa39241fb141c9ff1d9bdabbb6659dcc06f28f76
Shannon Trainor <sltrainor@outlook.com>
2020-08-08 12:50:32 -0400

    added function for getting results

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TableHeader from "./table-header";
+import TableRow from "./table-row";
 
 
 function Table() {
+    const [users, setUsers] = useState(null);
+    if (!users) {
+        fetch("https://randomuser.me/api/?results=10&nat=us").then(res => {
+            res.json().then(json => {
+                setUsers(json.results)
+            })
+        })
+    }
   return (
         <tbody>
-            <tr>
+            <TableHeader headings={["", "Name", "Email", "Phone Number"]} />
+            {users && users.map (item => {
+                return <TableRow imgSrc={item.picture.thumbnail}
+                name={`${item.name.first} ${item.name.last}`}
+                email={`${item.email}`}
+                phone={`${item.phone}`}
+                />
+            }
+            )}
+            {/* <tr>
                 <td>Image</td>
             </tr>
             <tr>
-                <td>name</td>
+                <td>Name</td>
             </tr>
             <tr>
                 <td>Email</td>
             </tr>
             <tr>
                 <td>Phone</td>
-            </tr>
+            </tr> */}
         </tbody>
 
   );
