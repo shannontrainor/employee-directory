07182a67f00e843b62742d9717c163a74f06b23e
Shannon Trainor <sltrainor@outlook.com>
2020-08-08 12:50:48 -0400

    added axios.get

--- src/utlis/API.js
+++ src/utlis/API.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export default {
-
+    //API call to get random users
     getUsers: function() {
-        return axios.get("https://randomuser.me/api/?results=100&nat=us")
+        return axios.get("https://randomuser.me/api/?results=10&nat=us")
     }
 }
